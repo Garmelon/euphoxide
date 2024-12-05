d22ccd767ae58af95eb0b133973caa0e4f320186
Joscha <joscha@plugh.de>
2024-12-05 11:30:32 +0100

    Prepare rewrite

--- src/bot.rs
+++
@@ -1,7 +0,0 @@
-//! Building blocks for bots.
-
-pub mod botrulez;
-pub mod command;
-pub mod commands;
-pub mod instance;
-pub mod instances;
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,1 @@
-pub mod api;
-#[cfg(feature = "bot")]
-pub mod bot;
-pub mod conn;
-mod emoji;
-pub mod nick;
-mod replies;
 
-pub use emoji::Emoji;
