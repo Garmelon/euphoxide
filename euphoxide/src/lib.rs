abcdb6b72e0b25efe89d6606aeac85201090a889
Joscha <joscha@plugh.de>
2025-03-20 08:37:55 +0100

    Document euphoxide crate

--- euphoxide/src/lib.rs
+++ euphoxide/src/lib.rs
@@ -1,3 +1,14 @@
+//! API bindings for [euphoria.leet.nu](https://euphoria.leet.nu/).
+//!
+//! If you want to write a generic client, see the `euphoxide-client` crate for
+//! connection management. If you want to write a bot, the `euphoxide-bot` crate
+//! contains additional useful tools.
+//!
+//! # Useful links
+//!
+//! - the [euphoria.leet.nu API docs](https://euphoria.leet.nu/heim/api)
+//! - [heim](https://github.com/CylonicRaider/heim), the software underlying euphoria
+
 pub mod api;
 pub mod client;
 pub mod conn;
